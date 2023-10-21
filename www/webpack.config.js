dcdf830a71efb4e930617b3d40c89ca51b930026
Daniel Vieira <danielvie@gmail.com>
2023-10-21 21:05:12 +0200

    update

--- www/webpack.config.js
+++ www/webpack.config.js
@@ -1,11 +1,21 @@
-
-const path = require('path')
+const path = require("path");
+const CopyWebpackPlugin = require("copy-webpack-plugin");
 
 module.exports = {
-    entry: "./index.js",
-    output: {
-        path: path.resolve(__dirname, "public"),
-        filename: "index.js"
-    },
-    mode: "development"
-}
+  entry: "./index.js",
+  output: {
+    path: path.resolve(__dirname, "public"),
+    filename: "index.js",
+  },
+  mode: "development",
+  plugins: [
+    new CopyWebpackPlugin({
+      patterns: [
+        {
+          from: "./index.html",
+          to: "./",
+        },
+      ],
+    }),
+  ],
+};
