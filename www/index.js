0e52c679d85fed13a89e14065e2afd99bc4699cf
Daniel Vieira <danielvie@gmail.com>
2023-10-21 22:11:11 +0200

    init wasm rust

--- www/index.js
+++ www/index.js
@@ -1,33 +1,5 @@
+import init, { greet } from "snake_game"
 
-async function init() {
-
-  const memory = new WebAssembly.Memory({initial: 1})
-
-  const importObject = {
-    js: {
-      mem: memory
-    },
-    console: {
-      log: () => {
-        console.log("just logging something!");
-      },
-      error: () => {
-        console.log("I am just error");
-      },
-    },
-  };
-
-  const response = await fetch("sum3.wasm");
-  const buffer = await response.arrayBuffer();
-  const wasm = await WebAssembly.instantiate(buffer, importObject);
-
-  const sumFunction = wasm.instance.exports.sum
-  const uint8Array = new Uint8Array(memory.buffer, 0, 7)
-  const hiText = new TextDecoder().decode(uint8Array)  
-  console.log(hiText)
-
-  const result = sumFunction(300, 5999);
-  console.log(result);
-}
-
-init();
+init().then(_ => {
+  greet("daniel")
+})
