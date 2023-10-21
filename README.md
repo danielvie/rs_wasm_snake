25f172ff9d8af4f021dab9b80fccd784e329926a
Daniel Vieira <danielvie@gmail.com>
2023-10-21 22:48:42 +0200

    with allocator to reduce size

--- README.md
+++ README.md
@@ -8,3 +8,7 @@ cargo install wasm-pack
 # commands
 
 wasm-pack build --target web
+
+# notes
+
+https://github.com/rustwasm/wee_alloc
--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,9 @@
 use wasm_bindgen::prelude::*;
 
+// Use `wee_alloc` as the global allocator.
+#[global_allocator]
+static ALLOC: wee_alloc::WeeAlloc = wee_alloc::WeeAlloc::INIT;
+
 #[wasm_bindgen]
 pub fn greet(name: &str) {
     // println!("Hi there {}", name);
